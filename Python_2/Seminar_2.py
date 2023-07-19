d82cb1b7dfe1bfe061026162be8a93e0d33a429e
Sofia Ergenova <113054061+SofiaErgenova@users.noreply.github.com>
2023-07-19 17:19:19 +0300

    Add files via upload

---
+++ Python_2/Seminar_2.py
@@ -0,0 +1,15 @@
+""" txt = input("Введите любой текст: ")
+print(type(txt),id(txt), hash(txt)) """
+
+""" txt = input("Введите любой текст: ")
+
+if txt.isdigit():
+    num = int(txt, base=10) 
+    print(bin(num), oct(num), hex(num))
+else:
+    print(txt.isascii())
+ """
+
+
+
+

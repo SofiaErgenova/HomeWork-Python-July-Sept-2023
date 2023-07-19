023ecf4d599c2fbca96352595bb09b2bccf8096f
Sofia Ergenova <113054061+SofiaErgenova@users.noreply.github.com>
2023-07-19 17:13:25 +0300

    Update HW-1.py

--- HW-1.py
+++ HW-1.py
@@ -103,18 +103,18 @@ a = 5
 b = -10
 c = -400
 
-d = b**2 - 4*a*c
+d = b ** 2 - 4 * a * c
 
 if d > 0:
-    x1 = (-b + math.sqrt(d))/(2*a)
-    x2 = (-b - math.sqrt(d))/(2*a)
+    x1 = (-b + math.sqrt(d))/(2 * a)
+    x2 = (-b - math.sqrt(d))/(2 * a)
 elif d == 0:
-    x1 = -b/(2*a)
+    x1 = -b/(2 * a)
     x2 = x1
 else:
     x1 = None
     x2 = None
 
-print("x1 =", x1)
-print("x2 =", x2)
+print("x1 = ", x1)
+print("x2 = ", x2)
 """
